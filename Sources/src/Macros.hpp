a573b055e2f8a90d6bc5b62a1dcf77042869ac83
Filip Stupka <filipstupka420@gmail.com>
2023-10-24 19:59:37 +0200

    #2: add useful macros

---
+++ Sources/src/Macros.hpp
@@ -0,0 +1,11 @@
+#ifndef MACROS_HPP
+#define MACROS_HPP
+
+/**
+ * @brief mark variable as unused
+ * 
+ * shuts down compiler warnings 
+ */
+#define MARK_UNUSED(x) ((void)(x))
+
+#endif // MACROS_HPP
