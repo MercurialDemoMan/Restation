71efa29c96d9f933a9a183604876581c20539a2b
Filip Stupka <filipstupka420@gmail.com>
2023-10-24 20:01:14 +0200

    #2: add data units

---
+++ Sources/src/Constants.hpp
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include "Types.hpp"
+
+namespace PSX
+{
+    /**
+     * data units
+     */
+    constexpr const u32 KiB = 1024;
+    constexpr const u32 MiB = KiB * KiB;
+    constexpr const u32 GiB = MiB * MiB;
+}
+
+#endif // CONSTANTS_HPP
