f324ab77a065d7c12d307a92154581ca99370544
Filip Stupka <filipstupka420@gmail.com>
2023-10-25 20:48:30 +0200

    #2: add file header documentation

--- Sources/src/Utils.hpp
+++ Sources/src/Utils.hpp
@@ -1,3 +1,36 @@
+/**
+ * @file      Utils.hpp
+ *
+ * @author    Filip Stupka \n
+ *            xstupk05@fit.vutbr.cz
+ *
+ * @brief     Bit manipulation and arithmetic utilities
+ *
+ * @version   0.1
+ *
+ * @date      25. 10. 2023, 20:39 (created)
+ *
+ * @section   TODO: replace with actual documentation
+ * TODO: documentation text
+ *
+ * @section License
+ * This file is part of the TODO: project \n
+ *
+ * Copyright (C) 2023
+ *
+ * This file is part of TODO: project. TODO: project is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TODO: project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TODO: project. If not, see http://www.gnu.org/licenses/.
+ */
+
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
