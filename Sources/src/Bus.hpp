55a21684e0bda077a921d7355bb132940c9dd5ee
Filip Stupka <filipstupka420@gmail.com>
2023-10-26 15:33:44 +0200

    #2: create bus header

---
+++ Sources/src/Bus.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file      Bus.hpp
+ *
+ * @author    Filip Stupka \n
+ *            xstupk05@fit.vutbr.cz
+ *
+ * @brief     PSX Bus header
+ *
+ * @version   0.1
+ *
+ * @date      26. 10. 2023, 15:32 (created)
+ *
+ * @section   TODO: replace with actual documentation
+ * TODO: documentation text
+ *
+ * @section License
+ * This file is part of the TODO: project \n
+ *
+ * Copyright (C) 2023
+ *
+ * This file is part of TODO: project. TODO: project is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TODO: project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TODO: project. If not, see http://www.gnu.org/licenses/.
+ */
+
+#ifndef BUS_HPP
+#define BUS_HPP
+
+#include "Types.hpp"
+#include "Constants.hpp"
+
+namespace PSX
+{
+    /**
+     * @brief The core of the PSX. Its' main purpose is dispatching reads and writes.
+     */
+    class Bus
+    {
+    public:
+
+    private:
+    
+    };
+}
+
+#endif // BUS_HPP
