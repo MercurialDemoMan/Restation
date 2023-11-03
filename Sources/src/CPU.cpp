540d28ed245360721b31e970369d684e23017be4
Filip Stupka <filipstupka420@gmail.com>
2023-11-03 17:04:13 +0100

    #4: create instructions layout in the CPU

--- Sources/src/CPU.cpp
+++ Sources/src/CPU.cpp
@@ -33,6 +33,7 @@
 
 #include "CPU.hpp"
 #include "Bus.hpp"
+#include "Utils.hpp"
 
 namespace PSX
 {
@@ -58,5 +59,384 @@ namespace PSX
     void CPU::reset()
     {
         TODO();
+    }
+
+    /**
+     * @brief set program counter to a specific address
+     */
+    void CPU::set_program_counter(u32 address)
+    {
+        m_program_counter      = address;
+        m_program_counter_next = address + sizeof(CPUInstruction);
+    }
+
+    /**
+     * @brief set register to a specific value 
+     */
+    void CPU::set_register(u32 register_id, u32 value)
+    {
+        if(register_id == 0)
+            return;
+
+        m_register_field[register_id] = value;
+
+        if(m_load_delay_slots[0].register_id == register_id)
+        {
+            TODO();
+        }
+    }
+
+    /**
+     * @brief make jump and set status flags
+     */
+    void CPU::do_jump(u32 address)
+    {
+        m_program_counter_next = address;
+        m_branching            = true;
+    }
+    
+    void CPU::UNK(const CPUInstruction&)
+    {
+        UNREACHABLE();
+    }
+
+    void CPU::FUN(const CPUInstruction& ins)
+    {
+        
+    }
+
+    void CPU::B(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::J(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::JAL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::BEQ(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::BNE(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::BLEZ(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::BGTZ(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::ADDI(const CPUInstruction& ins)
+    {
+        
+    }
+
+    void CPU::ADDIU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLTI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLTIU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::ANDI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::ORI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::XORI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LUI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::COP0(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::COP1(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::COP2(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::COP3(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LB(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LH(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LW(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LBU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LHU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SB(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SH(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SW(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWC0(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWC1(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWC2(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::LWC3(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWC0(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWC1(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWC2(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SWC3(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SRL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SRA(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLLV(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SRLV(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SRAV(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::JR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::JALR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SYSCALL(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::BREAK(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MFHI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MTHI(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MFLO(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MTLO(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MULT(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::MULTU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::DIV(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::DIVU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::ADD(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::ADDU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SUB(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SUBU(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::AND(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::OR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::XOR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::NOR(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLT(const CPUInstruction&)
+    {
+
+    }
+
+    void CPU::SLTU(const CPUInstruction&)
+    {
+
     }
 }
--- Sources/src/CPU.hpp
+++ Sources/src/CPU.hpp
@@ -37,6 +37,7 @@
 #include <memory>
 #include "Component.hpp"
 #include "Forward.hpp"
+#include "CPUInstruction.hpp"
 
 namespace PSX
 {
@@ -62,7 +63,116 @@ namespace PSX
 
     private:
 
-        std::shared_ptr<Bus> m_bus;
+        /**
+         * base instructions
+         */
+        void UNK(const CPUInstruction&);
+        void FUN(const CPUInstruction&);
+        void B(const CPUInstruction&);
+        void J(const CPUInstruction&);
+        void JAL(const CPUInstruction&);
+        void BEQ(const CPUInstruction&);
+        void BNE(const CPUInstruction&);
+        void BLEZ(const CPUInstruction&);
+        void BGTZ(const CPUInstruction&);
+        void ADDI(const CPUInstruction&);
+        void ADDIU(const CPUInstruction&);
+        void SLTI(const CPUInstruction&);
+        void SLTIU(const CPUInstruction&);
+        void ANDI(const CPUInstruction&);
+        void ORI(const CPUInstruction&);
+        void XORI(const CPUInstruction&);
+        void LUI(const CPUInstruction&);
+        void COP0(const CPUInstruction&);
+        void COP1(const CPUInstruction&);
+        void COP2(const CPUInstruction&);
+        void COP3(const CPUInstruction&);
+        void LB(const CPUInstruction&);
+        void LH(const CPUInstruction&);
+        void LWL(const CPUInstruction&);
+        void LW(const CPUInstruction&);
+        void LBU(const CPUInstruction&);
+        void LHU(const CPUInstruction&);
+        void LWR(const CPUInstruction&);
+        void SB(const CPUInstruction&);
+        void SH(const CPUInstruction&);
+        void SWL(const CPUInstruction&);
+        void SW(const CPUInstruction&);
+        void SWR(const CPUInstruction&);
+        void LWC0(const CPUInstruction&);
+        void LWC1(const CPUInstruction&);
+        void LWC2(const CPUInstruction&);
+        void LWC3(const CPUInstruction&);
+        void SWC0(const CPUInstruction&);
+        void SWC1(const CPUInstruction&);
+        void SWC2(const CPUInstruction&);
+        void SWC3(const CPUInstruction&);
+        
+        /**
+         * extended function instructions
+         */
+        void SLL(const CPUInstruction&);
+        void SRL(const CPUInstruction&);
+        void SRA(const CPUInstruction&);
+        void SLLV(const CPUInstruction&);
+        void SRLV(const CPUInstruction&);
+        void SRAV(const CPUInstruction&);
+        void JR(const CPUInstruction&);
+        void JALR(const CPUInstruction&);
+        void SYSCALL(const CPUInstruction&);
+        void BREAK(const CPUInstruction&);
+        void MFHI(const CPUInstruction&);
+        void MTHI(const CPUInstruction&);
+        void MFLO(const CPUInstruction&);
+        void MTLO(const CPUInstruction&);
+        void MULT(const CPUInstruction&);
+        void MULTU(const CPUInstruction&);
+        void DIV(const CPUInstruction&);
+        void DIVU(const CPUInstruction&);
+        void ADD(const CPUInstruction&);
+        void ADDU(const CPUInstruction&);
+        void SUB(const CPUInstruction&);
+        void SUBU(const CPUInstruction&);
+        void AND(const CPUInstruction&);
+        void OR(const CPUInstruction&);
+        void XOR(const CPUInstruction&);
+        void NOR(const CPUInstruction&);
+        void SLT(const CPUInstruction&);
+        void SLTU(const CPUInstruction&);
+    
+        /**
+         * base instruction handler map
+         */
+
+        typedef void(CPU::*OpcodeHandler)(const CPUInstruction&);
+        OpcodeHandler m_base_handlers[64] =
+        {
+            &CPU::FUN,  &CPU::B,     &CPU::J,    &CPU::JAL,   &CPU::BEQ,  &CPU::BNE, &CPU::BLEZ, &CPU::BGTZ,
+            &CPU::ADDI, &CPU::ADDIU, &CPU::SLTI, &CPU::SLTIU, &CPU::ANDI, &CPU::ORI, &CPU::XORI, &CPU::LUI,
+            &CPU::COP0, &CPU::COP1,  &CPU::COP2, &CPU::COP3,  &CPU::UNK,  &CPU::UNK, &CPU::UNK,  &CPU::UNK,
+            &CPU::UNK,  &CPU::UNK,   &CPU::UNK,  &CPU::UNK,   &CPU::UNK,  &CPU::UNK, &CPU::UNK,  &CPU::UNK,
+            &CPU::LB,   &CPU::LH,    &CPU::LWL,  &CPU::LW,    &CPU::LBU,  &CPU::LHU, &CPU::LWR,  &CPU::UNK,
+            &CPU::SB,   &CPU::SH,    &CPU::SWL,  &CPU::SW,    &CPU::UNK,  &CPU::UNK, &CPU::SWR,  &CPU::UNK,
+            &CPU::LWC0, &CPU::LWC1,  &CPU::LWC2, &CPU::LWC3,  &CPU::UNK,  &CPU::UNK, &CPU::UNK,  &CPU::UNK,
+            &CPU::SWC0, &CPU::SWC1,  &CPU::SWC2, &CPU::SWC3,  &CPU::UNK,  &CPU::UNK, &CPU::UNK,  &CPU::UNK,
+        };
+        
+        /**
+         * extended function instruction handler map
+         */
+        OpcodeHandler m_extended_function_handlers[64] =
+        {
+            &CPU::SLL,  &CPU::UNK,   &CPU::SRL,  &CPU::SRA,  &CPU::SLLV,    &CPU::UNK,   &CPU::SRLV, &CPU::SRAV,
+            &CPU::JR,   &CPU::JALR,  &CPU::UNK,  &CPU::UNK,  &CPU::SYSCALL, &CPU::BREAK, &CPU::UNK,  &CPU::UNK,
+            &CPU::MFHI, &CPU::MTHI,  &CPU::MFLO, &CPU::MTLO, &CPU::UNK,     &CPU::UNK,   &CPU::UNK,  &CPU::UNK,
+            &CPU::MULT, &CPU::MULTU, &CPU::DIV,  &CPU::DIVU, &CPU::UNK,     &CPU::UNK,   &CPU::UNK,  &CPU::UNK,
+            &CPU::ADD,  &CPU::ADDU,  &CPU::SUB,  &CPU::SUBU, &CPU::AND,     &CPU::OR,    &CPU::XOR,  &CPU::NOR,
+            &CPU::UNK,  &CPU::UNK,   &CPU::SLT,  &CPU::SLTU, &CPU::UNK,     &CPU::UNK,   &CPU::UNK,  &CPU::UNK,
+            &CPU::UNK,  &CPU::UNK,   &CPU::UNK,  &CPU::UNK,  &CPU::UNK,     &CPU::UNK,   &CPU::UNK,  &CPU::UNK,
+            &CPU::UNK,  &CPU::UNK,   &CPU::UNK,  &CPU::UNK,  &CPU::UNK,     &CPU::UNK,   &CPU::UNK,  &CPU::UNK
+        };
+
+        std::shared_ptr<Bus> m_bus;  /// connection to the bus
 
     };
 }
