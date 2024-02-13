193a1e42295423570835eb66e28c25bfaa49ffde
Filip Stupka <filipstupka420@gmail.com>
2024-02-13 10:36:43 +0100

    #8: add cdrom instruction

---
+++ Sources/src/CDROMInstruction.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file      CDROMInstruction.hpp
+ *
+ * @author    Filip Stupka \n
+ *            xstupk05@fit.vutbr.cz
+ *
+ * @brief     Header for the PSX CDROM Instruction
+ *
+ * @version   0.1
+ *
+ * @date      12. 2. 2023, 16:51 (created)
+ *
+ * @section   TODO: replace with actual documentation
+ * TODO: documentation text
+ *
+ * @section License
+ * This file is part of the TODO: project \n
+ *
+ * Copyright (C) 2023
+ *
+ * This file is part of TODO: project. TODO: project is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TODO: project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TODO: project. If not, see http://www.gnu.org/licenses/.
+ */
+
+#ifndef CDROMINSTRUCTION_HPP
+#define CDROMINSTRUCTION_HPP
+
+#include "Types.hpp"
+
+namespace PSX
+{
+    /**
+     * @brief PSX CDROM Instruction
+     */
+    union CDROMInstruction
+    {
+        CDROMInstruction() :
+            raw(0)
+        {
+            
+        }
+
+        CDROMInstruction(u8 value) :
+            raw(value)
+        {
+            
+        }
+
+        u8 raw; // raw representation of the instruction
+    };
+}
+
+#endif // CDROMINSTRUCTION_HPP
